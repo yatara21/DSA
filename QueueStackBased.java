9a638722e7838f0274b12dc21c0bed034a4ee2a9
yatara21 <122377462+yatara21@users.noreply.github.com>
2023-09-03 20:44:24 +0300

    Queue Stack-Based Implementation

---
+++ QueueStackBased.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+public class QueueStackBased<T> {
+    private Stack<T> stk1 ; //Encapsulates the stack till no one can access it. (OOP)
+    private Stack<T> stk2 ;
+    
+    public QueueStackBased(){
+        stk1 = new Stack<T>();
+        stk2 = new Stack<T>();    
+    }
+    public boolean isEmpty(){
+        return stk1.isEmpty() && stk2.isEmpty();
+    }
+    public int size(){
+        return stk1.size() + stk2.size();
+    }
+    public void enqueue(T t){ //any enqueue operation we deal with stk1
+        stk1.push(t);
+    }
+    public T dequeue(){  //any dequeue operation we pop all elements from stk1 and then deal with stk2.     
+        if (isEmpty()){
+            throw new RuntimeException("Queue is empty");
+        }
+        if (stk2.isEmpty()){
+            while (!stk1.isEmpty()){
+                stk2.push(stk1.pop());
+            }
+        }
+        return stk2.pop();
+        
+    }
+    public T peek(){
+        if (isEmpty()){
+            throw new RuntimeException("Queue is empty");
+        }
+        if (stk2.isEmpty()){
+            while (!stk1.isEmpty()){
+                stk2.push(stk1.pop());
+            }
+        }
+        return stk2.peek();
+    }
+    
+}
